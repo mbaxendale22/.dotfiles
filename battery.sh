dc06a53d31dd21961addf4ce7fd7086a21f0924a
Matthew Baxendale <89916796+mbaxendale22@users.noreply.github.com>
2022-10-04 13:24:58 +0100

    Add files via upload

---
+++ battery.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+battery_info=`ioreg -rc AppleSmartBattery`
+current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
+total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+
+percent_remaining=$(((current_charge/total_charge)*100))
+echo battery: $percent_remaining%
